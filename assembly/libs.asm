749aa70b3031f6305573c15180e448559a08bc69
proto17 <dmp250net@gmail.com>
2013-12-13 11:57:13 -0500

    assembly library

---
+++ assembly/libs.asm
@@ -0,0 +1,35 @@
+inc cpu.inc
+
+def ARG_STARTING_ADDRESS 0x3000
+def RET_STARTING_ADDRESS 0x2000
+
+jmp main
+
+main:
+  call adder16Test
+  jmp end
+
+adder16Test:
+  mova 0xff
+  movaf ARG_STARTING_ADDRESS
+  mova 0x03
+  movaf ARG_STARTING_ADDRESS + 1
+  mova 0x00
+  movaf ARG_STARTING_ADDRESS + 2
+  mova 0x44
+  movaf ARG_STARTING_ADDRESS + 3
+  call add16
+  ret
+
+  
+add16:
+  movfa ARG_STARTING_ADDRESSS + 1 ;load in the low byte of the first number
+  adda ARG_STARTING_ADDRESS + 3   ;add the low byte of the first with the low byte of the second
+  movaf RET_STARTING_ADDRESS + 1  ;move the result to the low byte of the return address
+  movfa ARG_STARTING_ADDRESS      ;load in the high byte of the first number
+  addca ARG_STARTING_ADDRESS + 2  ;add the high byte of the first with the low byte of the second (and add the carry bit)
+  movaf RET_STARTING_ADDRESS      ;move the result tp the high byte of the return address
+  ret                             ;return
+
+end: 
+  noop
