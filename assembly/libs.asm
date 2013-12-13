1c7743027e952746949bd2c0f10fbe957ffc5ba8
proto17 <dmp250net@gmail.com>
2013-12-13 12:11:31 -0500

    Update libs.asm

--- assembly/libs.asm
+++ assembly/libs.asm
@@ -1,5 +1,3 @@
-inc cpu.inc
-
 def ARG_STARTING_ADDRESS 0x3000
 def RET_STARTING_ADDRESS 0x2000
 
@@ -21,7 +19,33 @@ adder16Test:
   call add16
   ret
 
-  
+add24:
+  movfa ARG_STARTING_ADDRESSS + 2
+  adda ARG_STARTING_ADDRESSS + 5
+  movaf RET_STARTING_ADDRESS + 2
+  movfa ARG_STARTING_ADDRESSS + 1
+  addca ARG_STARTING_ADDRESSS + 4
+  movaf RET_STARTING_ADDRESS + 1
+  movfa ARG_STARTING_ADDRESSS
+  addca ARG_STARTING_ADDRESSS + 3
+  movaf RET_STARTING_ADDRESS
+  ret
+
+add32:
+  movfa ARG_STARTING_ADDRESSS + 3
+  adda ARG_STARTING_ADDRESSS + 6
+  movaf RET_STARTING_ADDRESS + 3
+  movfa ARG_STARTING_ADDRESSS + 2
+  addca ARG_STARTING_ADDRESSS + 5
+  movaf RET_STARTING_ADDRESS + 2
+  movfa ARG_STARTING_ADDRESSS + 1
+  addca ARG_STARTING_ADDRESSS + 4
+  movaf RET_STARTING_ADDRESS + 1
+  movfa ARG_STARTING_ADDRESSS
+  addca ARG_STARTING_ADDRESSS + 3
+  movaf RET_STARTING_ADDRESS
+  ret
+
 add16:
   movfa ARG_STARTING_ADDRESSS + 1 ;load in the low byte of the first number
   adda ARG_STARTING_ADDRESS + 3   ;add the low byte of the first with the low byte of the second
