fd998b0dd01029b63127a15069c9e3de52dcef8f
main <main@new-host.home>
2014-02-25 18:12:41 -0500

    Now using generics

--- sram.vhd
+++ sram.vhd
@@ -2,14 +2,18 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
 entity sram is
+	 Generic ( avail_addr_width : integer := 17;
+	           actual_addr_width : integer := 24;
+				  data_width : integer := 8
+	 );
     Port ( clk      : in   STD_LOGIC;
            rst      : in   STD_LOGIC;
            si       : in   STD_LOGIC;
-           data_in  : in   STD_LOGIC_VECTOR (7 downto 0);
+           data_in  : in   STD_LOGIC_VECTOR (data_width-1 downto 0);
            start    : in   STD_LOGIC;
            we       : in   STD_LOGIC;
-           addr     : in   STD_LOGIC_VECTOR (23 downto 0);
-           data_out : out  STD_LOGIC_VECTOR (7 downto 0);
+           addr     : in   STD_LOGIC_VECTOR (avail_addr_width-1 downto 0);
+           data_out : out  STD_LOGIC_VECTOR (data_width-1 downto 0);
            so       : out  STD_LOGIC;
            done     : out  STD_LOGIC;
            sck      : out  STD_LOGIC;
@@ -17,14 +21,14 @@ entity sram is
 end sram;
 
 architecture Behavioral of sram is
-	constant WRITE_INSTR : std_logic_vector(7 downto 0) := x"02";
-	constant READ_INSTR : std_logic_vector(7 downto 0) := x"03";
+	constant WRITE_INSTR : std_logic_vector(data_width-1 downto 0) := x"02";
+	constant READ_INSTR : std_logic_vector(data_width-1 downto 0) := x"03";
 
 	type state_t is (IDLE, INSTR, ADDRESS, DATA);
 	signal state : state_t := IDLE;
 
 	signal data_in_buffer : std_logic_vector(data_in'range);
-	signal addr_buffer : std_logic_vector(addr'range);
+	signal addr_buffer : std_logic_vector(actual_addr_width-1 downto 0);
 	signal we_buffer : std_logic;
 	signal start_buffer : std_logic := '0';
 	
@@ -48,7 +52,7 @@ begin
 					when IDLE =>
 						cs_n <= '1';
 						done <= '1';
-						bit_pos <= 7;
+						bit_pos <= data_width-1;
 						so <= '0';
 						sck_flag <= "00";
 						
@@ -57,7 +61,10 @@ begin
 							done <= '0';
 							cs_n <= '0';
 							data_in_buffer <= data_in;
-							addr_buffer <= addr;
+							if(actual_addr_width > avail_addr_width) then
+								addr_buffer(actual_addr_width - 1 downto avail_addr_width) <= (others => '0');
+							end if;
+							addr_buffer(avail_addr_width-1 downto 0) <= addr;
 							we_buffer <= we;
 						end if;
 						
@@ -72,7 +79,7 @@ begin
 						
 						if(bit_pos = 0) then
 							state <= ADDRESS;
-							bit_pos <= 23;
+							bit_pos <= actual_addr_width-1;
 						else
 							bit_pos <= bit_pos - 1;
 						end if;
@@ -83,7 +90,7 @@ begin
 						
 						if(bit_pos = 0) then
 							state <= DATA;
-							bit_pos <= 7;
+							bit_pos <= data_width-1;
 						else
 							bit_pos <= bit_pos - 1;
 						end if;
@@ -98,7 +105,7 @@ begin
 						
 						if(bit_pos = 0) then
 							state <= IDLE;
-							bit_pos <= 7;
+							bit_pos <= data_width-1;
 						else
 							bit_pos <= bit_pos - 1;
 						end if;
